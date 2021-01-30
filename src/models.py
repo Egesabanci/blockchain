f161865432cca4aad0c152063deb8f25c49bcb57
Ege Sabancı <egesabanci@outlook.com.tr>
2021-01-31 01:54:26 +0300

    updated Blockchain model class

--- src/models.py
+++ src/models.py
@@ -1,14 +1,15 @@
-import datetime
+import datetime.datetime as time
+from uuid import uuid4
 
 from .database import db
 
 # main database model - blockchain class
 class Blockchain(db.Model):
 	block_number = db.Column(db.Integer, primary_key = True)
-	block_id		 = db.Column(db.String(32), unique = True, nullable = False)
+	block_id		 = db.Column(db.String(32), unique = True, nullable = False, default = uuid4().hex)
 	block_hash	 = db.Column(db.String(64), unique = True, nullable = False)
 	block_data	 = db.Column(db.String(255), nullable = False)
-	block_date 	 = db.Column(db.String(26), default = datetime.datetime.now())
+	block_date 	 = db.Column(db.String(26), default = time.now())
 
 	def __repr__(self):
 		return f"<Block {self.block_id}>"
