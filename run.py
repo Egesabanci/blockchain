54590f0dc97d31f4d922783f7a335aa538dbddaf
Ege Sabancı <egesabanci@outlook.com.tr>
2021-02-01 00:33:23 +0300

    updated run.py

--- run.py
+++ run.py
@@ -5,19 +5,13 @@ from src.app import cfg
 from src.database import db
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--custom", action = "store_true")
 parser.add_argument("--host", type = str)
 parser.add_argument("--port", type = int)
 args = parser.parse_args()
 
 # runserver configuration
-host = cfg["host"]
-port = cfg["port"] 
-
-# if added custom arguments from commandline
-if args.custom:
-	host = args.host
-	port = args.port
+host = cfg["host"] if args.host is None else args.host
+port = cfg["port"] if args.port is None else args.port
 
 # run the server
 if __name__ == "__main__":
