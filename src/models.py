2fa0ef2ced9fd88cc31802cfe685b62fce39685c
Ege Sabancı <egesabanci@outlook.com.tr>
2021-01-31 18:33:45 +0300

    fixed typo models.py

--- src/models.py
+++ src/models.py
@@ -6,10 +6,10 @@ from .database import db
 # main database model - blockchain class
 class Blockchain(db.Model):
 	block_number = db.Column(db.Integer, primary_key = True)
-	block_id		 = db.Column(db.String(32), unique = True, nullable = False, default = uuid4().hex)
-	block_hash	 = db.Column(db.String(64), unique = True, nullable = False)
-	block_data	 = db.Column(db.String(255), nullable = False)
-	block_date 	 = db.Column(db.String(26), default = datetime.datetime.now())
+	block_id = db.Column(db.String(32), unique = True, nullable = False, default = uuid4().hex)
+	block_hash = db.Column(db.String(64), unique = True, nullable = False)
+	block_data = db.Column(db.String(255), nullable = False)
+	block_date = db.Column(db.String(26), default = datetime.datetime.now())
 
 	def __repr__(self):
 		return f"<Block {self.block_id}>"
